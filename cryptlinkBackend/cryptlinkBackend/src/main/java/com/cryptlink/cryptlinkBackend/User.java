8a82237e4a406482b83172c9f8c0aef66281e1c0
Mahmood Kidwai <mkidwai9248@gmail.com>
2025-02-24 16:53:16 -0500

    User.java added

---
+++ cryptlinkBackend/cryptlinkBackend/src/main/java/com/cryptlink/cryptlinkBackend/User.java
@@ -0,0 +1,45 @@
+package com.cryptlink.cryptlinkBackend;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+
+    // Getter and Setter for firstName
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // Getter and Setter for lastName
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // Getter and Setter for userName
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    // Getter and Setter for password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}    
