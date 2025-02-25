822540393503fd2d38d51a841b683cfb2202359c
kspark12 <kspark12@uncc.edu>
2025-02-25 16:38:20 -0500

    linking login and registration

--- cryptlinkapp/src/App.js
+++ cryptlinkapp/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-//import Registration from './Registration';
+import Registration from './registration';
 import Login from './login';
 
 function App() {
--- cryptlinkapp/src/registration.js
+++ cryptlinkapp/src/registration.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Registration.css";
+import "./registration.css";
 
 function Registration() {
   // State variables for each field
