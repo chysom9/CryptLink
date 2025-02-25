14bed2f9cafff02e40238e68ec6fa01284010180
HarshBhadania05 <hubhadania@gmail.com>
2025-02-25 17:11:09 -0500

    adding backend

---
+++ CryptLinkBackend/src/main/java/com/cryptLink/CryptLinkBackend/CryptLinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cryptLink.CryptLinkBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptLinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptLinkBackendApplication.class, args);
+	}
+
+}
---
+++ CryptLinkBackend/src/test/java/com/cryptLink/CryptLinkBackend/CryptLinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptLink.CryptLinkBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptLinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
