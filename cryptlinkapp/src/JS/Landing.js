b84ad5992f86bb7676f5cfdbe757b159368c0e16
kspark12 <kspark12@uncc.edu>
2025-03-24 20:14:29 -0400

    Update Landing.js

--- cryptlinkapp/src/JS/Landing.js
+++ cryptlinkapp/src/JS/Landing.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../css/landing.css";  
+
 import { ReactComponent as HomeLogo } from "../SVG/home_logo.svg";
 
 function Home() {
